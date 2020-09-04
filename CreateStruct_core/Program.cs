50b85a95a5e0ab0e7322556bf28958ff2da19d91
MixKage <shishov265@gmail.com>
2020-09-04 16:35:34 +0300

    Добавьте файлы проекта.

---
+++ CreateStruct_core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateStruct_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
